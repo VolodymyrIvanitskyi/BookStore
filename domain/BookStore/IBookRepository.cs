f65dc874d9d919ea9eb6cc581306eec59c951efe
Vova_Ivanitskyi <vovanhuk2003@gmail.com>
2020-12-22 18:08:50 +0200

    Add IBookRepository

---
+++ domain/BookStore/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore
+{
+    public interface IBookRepository
+    {
+        Book[] GetByTitle(string titlePart);
+    }
+}
