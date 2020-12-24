65eca43f4f74089f6c15c6fd7b19619d925e1041
Vova_Ivanitskyi <vovanhuk2003@gmail.com>
2020-12-24 11:30:54 +0200

    the search by the author and by ISBN is realized

---
+++ domain/BookStore.Tests/BookServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BookStore.Tests
+{
+    public class BookServiceTests
+    {
+        [Fact]
+        public void GetByQuery_WithIsbn_CallsGetByQuery()
+        {
+            var bookRepositoryStub = new Mock<IBookRepository>();
+
+            bookRepositoryStub.Setup(x => x.GetByIsbn(It.IsAny<string>()))
+                .Returns(new[] { new Book(1, "", "", "") });
+
+            bookRepositoryStub.Setup(x => x.GetByTitleOrAuthor(It.IsAny<string>()))
+                .Returns(new[] { new Book(2, "", "", "") });
+
+            BookService bookService = new BookService(bookRepositoryStub.Object);
+
+            var actual = bookService.GetByQuery("ISBN 1234-5678-90");
+
+            Assert.Collection(actual, book => Assert.Equal(1, book.Id));
+            
+        }
+        [Fact]
+        public void GetByQuery_WithAuthor_CallsGetByTitleOrAuthor()
+        {
+            var bookRepositoryStub = new Mock<IBookRepository>();
+
+            bookRepositoryStub.Setup(x => x.GetByIsbn(It.IsAny<string>()))
+                .Returns(new[] { new Book(1, "", "", "") });
+
+            bookRepositoryStub.Setup(x => x.GetByTitleOrAuthor(It.IsAny<string>()))
+                .Returns(new[] { new Book(2, "", "", "") });
+
+            BookService bookService = new BookService(bookRepositoryStub.Object);
+
+            var actual = bookService.GetByQuery("1234-5678-90");
+
+            Assert.Collection(actual, book => Assert.Equal(2, book.Id));
+
+        }
+    }
+}
